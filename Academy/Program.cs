0095f153be8f8453add761cff09e23add26397eb
ludvig.olofsson95@gmail.com <ludvig.olofsson95@gmail.com>
2023-10-28 15:08:59 +0200

    Spel färdigt

--- Academy/Program.cs
+++ Academy/Program.cs
@@ -1,7 +1,13 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
 using System.Threading.Tasks.Sources;
+using static System.Formats.Asn1.AsnWriter;
 
 var date = DateTime.UtcNow;
+
+var spel = new List<string>(); // lista obv
+
 string name = GetName();
 
 Menu(name);
@@ -19,62 +25,87 @@ void Menu(string? name)
     Console.WriteLine("-------------------------------------------------");
     Console.WriteLine($"Hej! {name}. Det är just nu: {date}. Detta är ett matematik spel.");
     Console.WriteLine("\n");
-    Console.WriteLine($@" Vilket typ av spel vill du göra idag? Välj mellan valen nedanför:
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
-Q - Avsluta");
-    Console.WriteLine("-------------------------------------------------");
 
-    var gameSelected = Console.ReadLine();
+    bool ärSpeletPå = true;
 
-    switch (gameSelected.Trim().ToLower())
+    do
     {
-        case "a":
-            AdditionGame("Addition spel vald");
-            Console.Clear();
-            break;
-        case "s":
-            SubtractionGame("Subtraction spel vald");
-            break;
-        case "m":
-            MultiplicationGame("Multiplication spel vald");
-            break;
-        case "d":
-            DivisionGame("Division spel vald");
-            break;
-        case "q":
-            Console.WriteLine("Hejdå");
-            Environment.Exit(1);
-            break;
-        default:
-            Console.WriteLine("Invalid input");
-            Environment.Exit(1);
-            break;
+        Console.Clear();
+        Console.WriteLine($@" Vilket typ av spel vill du göra idag? Välj mellan valen nedanför:
+                       V - Se tidigare spel
+                       A - Addition
+                       S - Subtraction
+                       M - Multiplication
+                       D - Division
+                       Q - Avsluta");
+        Console.WriteLine("-------------------------------------------------");
+
+        var gameSelected = Console.ReadLine();
+
+        switch (gameSelected.Trim().ToLower())
+        {
+            case "v":
+                HittaSpel();
+                break;
+            case "a":
+                AdditionGame("Addition spel vald");
+                Console.Clear();
+                break;
+            case "s":
+                SubtractionGame("Subtraction spel vald");
+                break;
+            case "m":
+                MultiplicationGame("Multiplication spel vald");
+                break;
+            case "d":
+                DivisionGame("Division spel vald");
+                break;
+            case "q":
+                Console.WriteLine("Hejdå");
+                ärSpeletPå = false;
+                Environment.Exit(1);
+                break;
+            default:
+                Console.WriteLine("Invalid input");
+                Environment.Exit(1);
+                break;
+        }
+
+    } while (ärSpeletPå);
+
+    
+}
+
+void HittaSpel()
+{
+    Console.Clear();
+    Console.WriteLine("Spel historik:");
+    Console.WriteLine("----------------------------");
+    foreach (var runda in spel)
+    {
+        Console.Write(runda);
     }
+    Console.WriteLine("----------------------------\n");
+    Console.WriteLine("Tryck på valfri tangent för att gå till Meny");
+    Console.ReadLine();
+
 }
 
 //kod för Division/Multi/Subtract/Addition
 void DivisionGame(string message)
 {
-    Console.WriteLine(message);
-
-    var random = new Random();
     var score = 0;
-
-    int firstNumber;
-    int secoundNumber;
-
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 5; i++)
     {
-        firstNumber = random.Next(1, 9);
-        secoundNumber = random.Next(1, 9);
+        Console.WriteLine(message);
+        var divisionNumbers = GetDivisionNumbers();
+        var firstNumber = divisionNumbers[0];
+        var secoundNumber = divisionNumbers[1];
 
         Console.WriteLine($"{firstNumber} / {secoundNumber}");
         var result = Console.ReadLine();
 
-        if (int.Parse(result) == firstNumber / secoundNumber)
+        if (int.Parse(result) == firstNumber * secoundNumber)
         {
             Console.WriteLine("Ditt svar var rätt");
             score++;
@@ -87,6 +118,9 @@ void DivisionGame(string message)
         if (i == 4) Console.WriteLine($"Game over. Du fick totalt {score} mycket poäng");
         Console.ReadLine();
     }
+
+    LäggTillHistorik(score, "division");
+
 }
 
 void MultiplicationGame(string message)
@@ -119,6 +153,8 @@ void MultiplicationGame(string message)
         if (i == 4) Console.WriteLine($"Game over. Du fick totalt {score} mycket poäng");
         Console.ReadLine();
     }
+
+    LäggTillHistorik(score, "Multiplication");
 }
 
 void SubtractionGame(string message)
@@ -152,6 +188,8 @@ void SubtractionGame(string message)
         if (i == 4) Console.WriteLine($"Game over. Du fick totalt {score} mycket poäng");
         Console.ReadLine();
     }
+
+    LäggTillHistorik(score, "Subtraction");
 }
 
 void AdditionGame(string message)
@@ -186,8 +224,13 @@ void AdditionGame(string message)
 
     }
 
-
+    LäggTillHistorik(score, "Addition");
  
+}
+
+void LäggTillHistorik(int spelScore, string spelTyp)
+{
+    spel.Add($"{DateTime.Now} - {spelTyp}: {spelScore} poäng.\n");
 }
 
 int[] GetDivisionNumbers()
@@ -196,11 +239,17 @@ int[] GetDivisionNumbers()
     var firstNumber = random.Next(1, 99);
     var secondNumber = random.Next(1, 99);
 
-
     var result = new int[2];
+    
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+    }
 
     result[0] = firstNumber;
     result[1] = secondNumber;
 
+   
     return result;
 }
